f5464f7b3bf435b23b0051dbf4584115307d851e
Rukaiya Bano <rukaiya.rk24@gmail.com>
2024-07-11 22:49:59 +0530

    Added logged in boolean for layout component

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React , { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import Profile from "../components/Profile";
 import "../app/globals.css";
 
 const SettingsPage: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token") || "";
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
   return (
     <div className="min-h-screen bg-[#1C1C1C]">
-      <Layout>
+      <Layout isLoggedIn={isLoggedIn}>
         <h1 className="mb-6 text-2xl font-medium text-white">
           Settings and Profile
         </h1>
