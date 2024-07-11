c7abb6e1b0a81ac7c57f57f72c3a06c8aa6b6c20
Rukaiya Bano <rukaiya.rk24@gmail.com>
2024-07-11 22:41:45 +0530

    Passes isloggedin boolean to console and changed console text

--- pages/pods.tsx
+++ pages/pods.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React , { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import MyPodsDashboard from "@/components/MyPodsDashboard";
 import AvailableCredits from "@/components/AvailableCredits";
 import "../app/globals.css";
 
 const MyPods: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token") || "";
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <div className="min-h-screen bg-[#1C1C1C]">
-      <Layout>
+      <Layout isLoggedIn={isLoggedIn}>
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-2xl font-medium text-white">My Pods</h1>
           <AvailableCredits />
