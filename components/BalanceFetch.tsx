cc5bf22d808312a00375bb2de272ab956c3b9861
Rukaiya Bano <rukaiya.rk24@gmail.com>
2024-07-12 22:27:29 +0530

    Use common API for api calls

--- components/BalanceFetch.tsx
+++ components/BalanceFetch.tsx
@@ -6,7 +6,7 @@ export interface BalanceData {
 
 export const fetchBalance = async (token: string): Promise<BalanceData> => {
 
-const response = await apiRequest<{ data: BalanceData }>("/api/v1/account/info", "POST", {}, true, true);
+const response = await apiRequest<{ data: BalanceData }>("/api/v1/account/info", "POST",true, true, {});
 
     if (!response || !response.data || response.data.balance === undefined) {
         throw new Error("Invalid response structure: " + JSON.stringify(response));
